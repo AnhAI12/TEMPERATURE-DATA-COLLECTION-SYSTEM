46e09f3a2364e8fe29a1ae3e27e3f8bbfb21669a
Duy Anh <106216724+AnhAI12@users.noreply.github.com>
2023-05-02 11:56:28 +0700

    Add files via upload

---
+++ website/readDataC.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+
+// dang nhap vao database
+include("config.php");
+
+// Doc gia tri RGB tu database
+//$sql = "select * from test_wt";
+$sql="select * from test_wt where stt>(select max(stt) from test_wt)-35";
+$result = mysqli_query($conn,$sql);
+
+$data = array();
+foreach ($result as $row){
+    $data[] = $row;
+}
+
+// add random data
+// $d1 = rand(20, 50);
+// $d2 = rand(10, 60);
+// $d3 = rand(10, 60);
+
+// $sql = "insert into chart(data1,data2,data3) values ($d1,$d2,$d3)";
+// mysqli_query($conn,$sql);
+
+mysqli_close($conn);
+echo json_encode($data);
+
+?>
